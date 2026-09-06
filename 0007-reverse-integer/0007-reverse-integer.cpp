2c8001056f8150eebbf25c6bda97d03ca6472c4f
Tanishq-Jindal <jindaltanishq96@gmail.com>
2026-09-06 18:49:01 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.44%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int ld = x % 10;
+
+            // Check overflow before rev * 10
+            if (rev > INT_MAX / 10 || 
+                (rev == INT_MAX / 10 && ld > 7)) {
+                return 0;
+            }
+
+            if (rev < INT_MIN / 10 || 
+                (rev == INT_MIN / 10 && ld < -8)) {
+                return 0;
+            }
+
+            rev = rev * 10 + ld;
+            x = x / 10;
+        }
+
+        return rev;
+    }
+};
